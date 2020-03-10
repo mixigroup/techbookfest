e91ef99eab630d495ee7ce496e60eae7d56369aa
MATSUBARA Nobutada <nobutada.matsubara@mixi.co.jp>
2020-03-10 21:48:06 +0900

    Fix: deprecated authentication for GitHub API in post issue comment script

--- template/script/post_pr_comment.rb
+++ template/script/post_pr_comment.rb
@@ -2,8 +2,8 @@ result = File.read(ARGV[0]).tr('"', "'")
 pr_num = ARGV[1]
 if pr_num =~ /^pr/
   endpoint = "api.github.com/repos/#{ARGV[2]}/issues/#{pr_num.tr('pr/','')}/comments"
-  url = "https:/#{endpoint}?access_token=#{ARGV[3]}"
+  url = "https:/#{endpoint}"
   json = "{\"body\":\"textlint でエラーがあったようです。\n~~~#{result}~~~\"}"
   puts "[POST] : #{endpoint} : #{json.inspect}"
-  `curl -X POST -H 'Content-Type:application/json' -d #{json.inspect} #{url}`
+  `curl -X POST -H 'Content-Type:application/json' -H 'Authorization: token #{ARGV[3]}' -d #{json.inspect} #{url}`
 end
